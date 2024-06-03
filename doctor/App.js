91c53e911faffe4fd6d7b49eefc9c8f5bd808a85
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-06-03 11:08:00 +0530

    update app js file

--- doctor/App.js
+++ doctor/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Screens/Home';
 import Onboardings from './Screens/Onboarding'
+import Board from './Screens/Board';
 
 
 const Stack = createNativeStackNavigator();
@@ -13,6 +14,7 @@ export default function App() {
       <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Onboarding" component={Onboardings}/>
+        <Stack.Screen name="Board" component={Board} />
       </Stack.Navigator>
     </NavigationContainer>
   );
