6d8ef3fe8dcaa7683af8e674bbce8cba75da87a5
dhananjana123 <warunad999@gmail.com>
2024-06-04 17:01:41 +0530

    add error showing notification

--- doctor/App.js
+++ doctor/App.js
@@ -1,25 +1,23 @@
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Screens/Home';
-import Onboardings from './Screens/Onboarding'
+import Onboardings from './Screens/Onboarding';
 import Board from './Screens/Board';
 import Preview from './Screens/Preview';
-
+import LastPage from './Screens/lastpage';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Onboarding" component={Onboardings}/>
-        <Stack.Screen name="Board" component={Board} />
-        <Stack.Screen name="ImagePreview" component={Preview} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+				<Stack.Screen name="Home" component={Home} />
+				<Stack.Screen name="Onboarding" component={Onboardings} />
+				<Stack.Screen name="Board" component={Board} />
+				<Stack.Screen name="ImagePreview" component={Preview} />
+				<Stack.Screen name="Lastpage" component={Lastpage} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
 }
-
-
