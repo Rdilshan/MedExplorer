4826822d24357b82e55c050b6fd2355de7a8f79d
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-06-04 13:34:58 +0530

    create bottom tab files

---
+++ doctor/src/Screens/Tabs/DHome.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function DHome() {
+  return (
+    <View>
+      <Text>DHome</Text>
+    </View>
+  )
+}
---
+++ doctor/src/Screens/Tabs/Prescription.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Prescription() {
+  return (
+    <View>
+      <Text>Prescription</Text>
+    </View>
+  )
+}
