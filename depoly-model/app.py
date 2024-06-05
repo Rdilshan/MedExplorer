9b91836a3b0b919cdee11f90af917f47182765a4
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-05 08:58:13 +0530

    create /prediction path and pass image

--- depoly-model/app.py
+++ depoly-model/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 app = Flask(__name__)
@@ -7,3 +7,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello, World!"
+
+
+@app.route("/prediction", methods=["POST"])
+def prediction():
+    if request.method == "POST":
+        if "file" not in request.files:
+            return "No file part in the request", 400
+        
+        file = request.files["file"]
+        file.save("image.jpg")
+       
+        return "Image received and processed successfully"
+    else:
+        return "Only POST requests are allowed for this endpoint", 405
+    
+if __name__ == "__main__":
+    app.run(debug=True)
