69c83d97f0f73a4bc8233f8f32e0e6648e4239fa
Randika Dilshan <rdilshan077788@gmail.com>
2024-06-07 13:27:17 +0530

    change initialRouteName  Dashboard

--- doctor/App.js
+++ doctor/App.js
@@ -6,19 +6,22 @@ import Board from './Screens/Board';
 import Preview from './Screens/Preview';
 import Wrong from './Screens/Wrong';
 import Success from './Screens/Success';
+import Dashboard from './Screens/Dashboard';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
+			<Stack.Navigator initialRouteName="Dashboard" screenOptions={{ headerShown: false }}>
 				<Stack.Screen name="Home" component={Home} />
 				<Stack.Screen name="Onboarding" component={Onboardings} />
 				<Stack.Screen name="Board" component={Board} />
 				<Stack.Screen name="ImagePreview" component={Preview} />
 				<Stack.Screen name="Wrong" component={Wrong} />
 				<Stack.Screen name="Success" component={Success} />
+				<Stack.Screen name="Dashboard" component={Dashboard} />
 			</Stack.Navigator>
 		</NavigationContainer>
 	);
