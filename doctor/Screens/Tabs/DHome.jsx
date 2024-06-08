c26cc3d82d96b9944eda96925f01ed2692e5f392
dhananjana123 <warunad999@gmail.com>
2024-06-08 08:57:23 +0530

    add home image and text

--- doctor/Screens/Tabs/DHome.jsx
+++ doctor/Screens/Tabs/DHome.jsx
@@ -1,10 +1,64 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Image } from "react-native";
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
 
-export default function DHome() {
+export default function HeaderComponent() {
   return (
-    <View>
-      <Text>DHome</Text>
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <View style={styles.row}>
+          <Image
+            source={require('../../assets/icon.png')}
+            style={styles.icon}
+          />
+          <Ionicons name="notifications" size={30} color="white" />
+        </View>
+        <Text style={styles.greeting}>Hello, Doctor</Text>
+      </View>
     </View>
-  )
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#f8f8f8',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  top: {
+    backgroundColor: "#0165FC",
+    paddingVertical: 65,
+    paddingHorizontal: 20,
+    borderBottomLeftRadius: 18,
+    borderBottomRightRadius: 18,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 8,
+    width: '100%',
+    alignItems: 'center', // Center contents horizontally
+  },
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: '100%',
+    paddingHorizontal: 20,
+  },
+  icon: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+  },
+  greeting: {
+    fontSize: 19,
+    color: 'white',
+    marginTop: 16, // Space between the row and the greeting text
+    marginRight:150
+  },
+});
