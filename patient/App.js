49a7a5d1a94f7bf749d3a1df99d8ec85e4dbaf8d
Bimsara <ravindu.bimsara97@gmail.com>
2024-06-21 08:36:05 +0530

    navigation maincontent

--- patient/App.js
+++ patient/App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SignIN from './screen/SignIn';
+import MainContainer from './screen/Maincontent';
 
 export default function App() {
   return (
-    <SignIN/>
+    <MainContainer/>
   );
 }
 
