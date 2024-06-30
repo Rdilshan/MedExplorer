0b8de1ab75d5418c392d1830bd9cf646de84aa9a
dhananjana123 <warunad999@gmail.com>
2024-06-30 07:35:06 +0530

    Add Notification page

--- patient/App.js
+++ patient/App.js
@@ -2,10 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SignIN from './screen/SignIn';
 import MainContainer from './screen/Maincontent';
-
+import Notification from './screen/Notification';
 export default function App() {
   return (
-    <MainContainer/>
+    // <MainContainer/>
+    <Notification/>
   );
 }
 
