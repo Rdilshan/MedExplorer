b860f7c9928bc1a2b4e42ee0eb1e3df532ee3254
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-06-30 12:40:46 +0530

    changes

--- doctor/Screens/Preview.jsx
+++ doctor/Screens/Preview.jsx
@@ -1,16 +1,31 @@
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
 
-import React from 'react';
-import { View, Text, Image, StyleSheet, Dimensions } from 'react-native';
-
-const { width, height } = Dimensions.get('window');
+const { width, height } = Dimensions.get("window");
 
 export default function Preview({ route }) {
   const { imageUri } = route.params;
+  const handleSendPress = () => {
+    console.log("Send button pressed");
+  };
 
   return (
     <View style={styles.previewContainer}>
-      <Text style={styles.previewText}>Image Preview:</Text>
+      <Text style={styles.previewText}>This is the Preview of Prescription</Text>
       <Image source={{ uri: imageUri }} style={styles.previewImage} />
+      <View style={{ display:"flex" , flexDirection:"row",gap:20}}>
+      <TouchableOpacity style={styles.sendButton} onPress={handleSendPress}>
+        <Text style={styles.sendButtonText}>Complete</Text>
+      </TouchableOpacity>
+     
+      </View>
     </View>
   );
 }
@@ -18,17 +33,27 @@ export default function Preview({ route }) {
 const styles = StyleSheet.create({
   previewContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   previewText: {
     fontSize: 18,
     marginBottom: 10,
+    fontWeight: "bold",
   },
   previewImage: {
     width: width * 0.9,
     height: height * 0.7,
-    borderColor: 'black',
+    borderColor: "black",
     borderWidth: 1,
+  },
+  sendButtonText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    backgroundColor: "green",
+    padding: 10,
+    marginVertical: 4,
+    borderRadius: 3,
   },
 });
