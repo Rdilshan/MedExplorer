e120a064bfae4fb2e944cd4171e0c455dccb1136
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-07-05 11:48:38 +0530

    set push notification to any patient app(comment the code)

--- doctor/Screens/Success.jsx
+++ doctor/Screens/Success.jsx
@@ -2,10 +2,33 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import React from 'react';
 import LottieView from 'lottie-react-native';
 import { useNavigation, useRoute } from "@react-navigation/native";
+// import axios from 'axios';
 export default function Success() {
     const navigation = useNavigation();
     const route = useRoute();
     const { patientName } = route.params;
+
+    // const handleClick = async (title) => {
+    //     const url = 'https://app.nativenotify.com/api/notification';
+    
+    //     const data = {
+    //         appId: 22292,
+    //         appToken: "0OCZaZsKvWSdD51gjla2bL",
+    //         title: "Push title here as a string",
+    //         body: "Push message here as a string",
+    //         dateSent: "7-5-2024 11:37AM",
+    //         pushData: { yourProperty: "yourPropertyValue" },
+    //         bigPictureURL: "Big picture URL as a string"
+    //     };
+    
+    //     try {
+    //       const response = await axios.post(url, data);
+    //       console.log(response.data);
+    //       navigation.navigate('Dashboard', { screen: 'Prescription', params: { patientName } });
+    //     } catch (error) {
+    //       console.error('Error:', error);
+    //     }
+    //   }
     return (
         <View style={styles.container}>
             <View style={styles.viewStyle}>
@@ -20,7 +43,9 @@ export default function Success() {
                     {'Before sending the prescription, here are \n all the details we attached'}
                 </Text>
                 <TouchableOpacity style={styles.buttonStyle} 
-               onPress={() => navigation.navigate('Dashboard', { screen: 'Prescription', params: { patientName } })}>
+                // onPress={handleClick}
+               onPress={() => navigation.navigate('Dashboard', { screen: 'Prescription', params: { patientName } })}
+               >
                     <Text style={styles.buttonText}>Continue</Text>
                 </TouchableOpacity>
             </View>
