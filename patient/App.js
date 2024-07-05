c9cd1944aafdff323487c78626090f81123b6f36
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-07-05 11:20:09 +0530

    set up to recive notification

--- patient/App.js
+++ patient/App.js
@@ -3,8 +3,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import SignIN from './screen/SignIn';
 import MainContainer from './screen/Maincontent';
 import Register from './screen/Register'
+import registerNNPushToken from 'native-notify';
+import { useEffect } from 'react';
+import { REGISTER_NN_PUSH_TOKEN_ID, REGISTER_NN_PUSH_TOKEN_SECRET } from '@env';
 
 export default function App() {
+  useEffect(() => {
+    registerNNPushToken(REGISTER_NN_PUSH_TOKEN_ID, REGISTER_NN_PUSH_TOKEN_SECRET);
+}, []);
   return (
     <Register/>
   );
