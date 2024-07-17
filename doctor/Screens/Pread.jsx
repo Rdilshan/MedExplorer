816b575a90613fc1aae67b43f04f4c20261fc799
Randika Dilshan <rdilshan077788@gmail.com>
2024-07-17 21:30:01 +0530

    prediction data coming and showing

--- doctor/Screens/Pread.jsx
+++ doctor/Screens/Pread.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from 'react';
-import { View, TextInput, StyleSheet, Dimensions } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, StyleSheet, Dimensions, TouchableOpacity, Text } from 'react-native';
+import { useNavigation, useRoute } from "@react-navigation/native";
 
 const { height: screenHeight } = Dimensions.get('window');
 
+
+
 export default function Pread() {
-    const [text, setText] = useState('Prescription details:\n\nDaily Medications:\n- Drug 1: Dosage\n- Drug 2: Dosage\n- Drug 3: Dosage');
+  const route = useRoute();
+  const { predicresult } = route.params;
+
+  const navigation = useNavigation();
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    if (predicresult) {
+      const formattedResult = predicresult.map((drug) => `${drug}`).join('\n');
+      const updatedText = `${text}\n${formattedResult}`;
+      setText(updatedText);
+    }
+  }, [predicresult]);
 
   return (
     <View style={styles.container}>
@@ -13,8 +28,12 @@ export default function Pread() {
         multiline={true}
         placeholder="Enter text here"
         value={text}
-        onChangeText={(newText) => setText(newText)} 
+        onChangeText={(newText) => setText(newText)}
       />
+
+      <TouchableOpacity style={styles.sendButton} onPress={() => navigation.goBack()}>
+        <Text >Done</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -31,6 +50,16 @@ const styles = StyleSheet.create({
     padding: 10,
     textAlignVertical: 'top',
     marginTop: 20,
-    fontSize:20
+    fontSize: 20
   },
+  sendButton: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    backgroundColor: "#33b249",
+    padding: 10,
+    marginVertical: 4,
+    borderRadius: 3,
+  },
+
 });
