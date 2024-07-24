1b3c3d09b4661f20a7e4455aa31161a8ae275f67
GThashan <124758477+tharindususl@users.noreply.github.com>
2024-07-24 19:21:25 +0530

    Web

---
+++ QR/Web/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline ">
+      Hello world!
+    </h1>
+    </>
+  )
+}
+
+export default App
