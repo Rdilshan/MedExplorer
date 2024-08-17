87a0d6e9700a79a4ddcb25fd41a12febabcb717f
dhananjana123 <warunad999@gmail.com>
2024-08-17 06:02:28 +0530

    add top-down

--- material-dashboard-react-main/src/layouts/profile/index.js
+++ material-dashboard-react-main/src/layouts/profile/index.js
@@ -1,7 +1,9 @@
-// @mui material components
+import React, { useState } from "react";
 import Grid from "@mui/material/Grid";
 import Divider from "@mui/material/Divider";
-import { useParams } from "react-router-dom";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
 
 // @mui icons
 import FacebookIcon from "@mui/icons-material/Facebook";
@@ -19,6 +21,12 @@ import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
 import Header from "layouts/profile/components/Header";
 
 function Overview() {
+  const [cimSelected, setCimSelected] = useState("CMI Num 1");
+
+  const handleCimSelectedChange = (event) => {
+    setCimSelected(event.target.value);
+  };
+
   return (
     <DashboardLayout>
       <MDBox mb={2} />
@@ -26,15 +34,28 @@ function Overview() {
         <MDBox mt={5} mb={3}>
           <Grid container spacing={1} justifyContent="center">
             <Grid item xs={12} md={8} xl={6} sx={{ display: "flex", justifyContent: "center" }}>
-              <Divider orientation="vertical" sx={{ ml: -2, mr: 1 }} />
+              <Divider orientation="vertical" sx={{ ml: -10, mr: 1 }} />
               <ProfileInfoCard
                 title="profile information"
-                description="Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
+                description="Hi, I’m Alec Thompson. Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
                 info={{
                   fullName: "Alec M. Thompson",
                   mobile: "(44) 123 1234 123",
                   email: "alecthompson@mail.com",
-                  location: "USA",
+                  cimSelected: (
+                    <FormControl sx={{ mt: -2.95, minWidth: 72 }} fullWidth>
+                      <Select
+                        labelId="cim-selected-label"
+                        id="cim-selected-select"
+                        value={cimSelected}
+                        onChange={handleCimSelectedChange}
+                        sx={{ height: 28 }}
+                      >
+                        <MenuItem value="CMI Num 1">CMI Num 1</MenuItem>
+                        <MenuItem value="CMI Num 2">CMI Num 2</MenuItem>
+                      </Select>
+                    </FormControl>
+                  ),
                 }}
                 social={[
                   {
